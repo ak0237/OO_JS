0fc5ad18b19274f3b1e0ebe17f34891dfa50eeb9
Andrey <andrey.karvat123@hotmail.com>
2022-02-18 15:21:21 -0300

    metodos com retorno js

--- index.js
+++ index.js
@@ -11,7 +11,16 @@ class ContaCorrente{
     sacar(valor){
         if(this.#saldo >= valor){ // this é usado para referenciar a conta que está chamando o metodo 
             this.#saldo -= valor;
+            return valor;
         }
+    }
+
+    depositar(valor){
+        if (valor <=0){ 
+            return;  
+        } // Earlier Return, facilita a leitura do código
+
+        this.#saldo += valor;
     }
 }
 
