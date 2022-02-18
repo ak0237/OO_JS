5b528cd519b100c13841f19bbf5eb0d0c5f1bff4
Andrey <andrey.karvat123@hotmail.com>
2022-02-18 15:07:38 -0300

    metodos js

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 class Cliente{
     nome;
     cpf;
+    
+}
+
+class ContaCorrente{
     agencia;
     saldo;
+
+    sacar(valor){
+        if(this.saldo >= valor){ // this é usado para referenciar a conta que está chamando o metodo 
+            this.saldo -= valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente(); //Cliente1 é uma instancia da classe cliente
 cliente1.nome = 'Rae';
 cliente1.cpf = 40028922;
-cliente1.agencia = 2214
+cliente1.agencia = 2214;
 cliente1.saldo;
 
 console.log(cliente1)
