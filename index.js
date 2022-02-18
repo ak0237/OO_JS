5bf55c3355aa49956f1b8328bfc4b9db3fb185cf
Andrey <andrey.karvat123@hotmail.com>
2022-02-18 15:15:23 -0300

    atributos privados js

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ class Cliente{
 
 class ContaCorrente{
     agencia;
-    saldo;
+    #saldo = 0; // # ou _ = privado
 
     sacar(valor){
-        if(this.saldo >= valor){ // this é usado para referenciar a conta que está chamando o metodo 
-            this.saldo -= valor;
+        if(this.#saldo >= valor){ // this é usado para referenciar a conta que está chamando o metodo 
+            this.#saldo -= valor;
         }
     }
 }
@@ -19,6 +19,5 @@ const cliente1 = new Cliente(); //Cliente1 é uma instancia da classe cliente
 cliente1.nome = 'Rae';
 cliente1.cpf = 40028922;
 cliente1.agencia = 2214;
-cliente1.saldo;
 
 console.log(cliente1)
