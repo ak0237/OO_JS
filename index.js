79daa0f17c415e4c33f45a337c6591d10e26eb42
Andrey <andrey.karvat123@hotmail.com>
2022-02-18 15:01:08 -0300

    classes e instancias js

---
+++ index.js
@@ -0,0 +1,14 @@
+class Cliente{
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+
+const cliente1 = new Cliente(); //Cliente1 é uma instancia da classe cliente
+cliente1.nome = 'Rae';
+cliente1.cpf = 40028922;
+cliente1.agencia = 2214
+cliente1.saldo;
+
+console.log(cliente1)
